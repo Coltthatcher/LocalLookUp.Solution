bc2b88df58d7cbfac7bf43e04266eda0bf862e6b
Colt Thatcher <colt.thatcher303@gmail.com>
2022-01-21 14:30:35 -0800

    Add ResturantsShops.Controller

--- LocalLookUp/Startup.cs
+++ LocalLookUp/Startup.cs
@@ -44,7 +44,7 @@ namespace LocalLookUp
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalLookUp v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
