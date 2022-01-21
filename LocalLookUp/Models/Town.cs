f90f4d23f904b81b7ef5dec6026127d8a9455644
Colt Thatcher <colt.thatcher303@gmail.com>
2022-01-21 15:49:34 -0800

    Remove Models/Shop.cs replace Models/Restuarnt.cs with Models/Town.cs

---
+++ LocalLookUp/Models/Town.cs
@@ -0,0 +1,11 @@
+namespace LocalLookUp.Models
+{
+    public class Town
+    {
+        public int TownId { get; set; }
+        public string Name { get; set; }
+        public string Restaurant { get; set; }
+        
+        public string Shop { get; set; }
+    }
+}
