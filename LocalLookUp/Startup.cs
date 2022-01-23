6d8cf2c147972f921bf7806ebde98b38d7dbf892
Colt Thatcher <colt.thatcher303@gmail.com>
2022-01-23 10:09:53 -0800

    Add Swagger by modifying my startup.cs file

--- LocalLookUp/Startup.cs
+++ LocalLookUp/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using LocalLookUp.Models;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace LocalLookUp
 {
@@ -24,7 +26,31 @@ public class Startup
             services.AddDbContext<LocalLookUpContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+
+                //Swagger
+            services.AddSwaggerGen(options =>
+                {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "LocalLookUp API",
+                Description = "An ASP.NET Core Web API for Seeing a Restaurant or Shop in one of the provided towns",
+                TermsOfService = new Uri("https://example.com/terms"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Example Contact",
+                    Url = new Uri("https://example.com/contact")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Example License",
+                    Url = new Uri("https://example.com/license")
+                }
+            });
+        });
+        
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,6 +61,13 @@ public class Startup
             }
 
             // app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
